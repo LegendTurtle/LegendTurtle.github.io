df4494a1c749d5ad86da71eefccc784c0a4b7ea7
Egor <legendturtle@gmail.com>
2025-02-11 18:49:07 +0300

    test

--- js-to-pdf/script.js
+++ js-to-pdf/script.js
@@ -1,29 +1,29 @@
-const JSdropZone = document.getElementById('js-input-zone')
-const JSinput = document.getElementById('js-input')
+const JSdropZone = document.getElementById("js-input-zone");
+const JSinput = document.getElementById("js-input");
 
-let js_file
+let js_file;
 
-let is_js_received
-let is_pdf_received
+let is_js_received;
+let is_pdf_received;
 
-document.addEventListener('dragover', ev => ev.preventDefault())
-document.addEventListener('drop', ev => ev.preventDefault())
+document.addEventListener("dragover", (ev) => ev.preventDefault());
+document.addEventListener("drop", (ev) => ev.preventDefault());
 
-function js_recieved(){
-    is_js_received = true
-    log(js_file)
+function js_recieved() {
+  is_js_received = true;
+  log(js_file);
 }
 
-JSdropZone.addEventListener('drop', ev => {
-    ev.preventDefault()
-    js_file = ev.dataTransfer.files[0]
-    js_recieved();
-})
+JSdropZone.addEventListener("drop", (ev) => {
+  ev.preventDefault();
+  js_file = ev.dataTransfer.files[0];
+  js_recieved();
+});
 
-JSdropZone.addEventListener('click', () => {
-    JSinput.click()
-    JSinput.addEventListener('change', () => {
-        js_file = JSinput.files[0]
-        js_recieved();
-    })
-})
+JSdropZone.addEventListener("click", () => {
+  JSinput.click();
+  JSinput.addEventListener("change", () => {
+    js_file = JSinput.files[0];
+    js_recieved();
+  });
+});
