1424f602452dc21e65a19f52cb32a45652596b68
Egor <legendturtle@gmail.com>
2025-02-11 22:09:38 +0300

    commit

--- js-to-pdf/script.js
+++ js-to-pdf/script.js
@@ -4,8 +4,13 @@ const JSinput = document.getElementById("js-input");
 let js_file;
 let pdf_file;
 
+function js_click() {
+  JSinput.click();
+}
 function js_receive() {
-  console.log("click");
+  js_file = JSinput.files[0];
+  console.log(js_file);
 }
 
-JSdropZone.addEventListener("click", js_receive);
+JSdropZone.addEventListener("click", js_click);
+JSinput.addEventListener("change", js_receive);
