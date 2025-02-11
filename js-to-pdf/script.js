f3263519d07d201a56e4c541e433100b78ae0fc3
Egor <legendturtle@gmail.com>
2025-02-11 22:48:17 +0300

    main

--- js-to-pdf/script.js
+++ js-to-pdf/script.js
@@ -1,5 +1,7 @@
 const JSdropZone = document.getElementById("js-input-zone");
 const JSinput = document.getElementById("js-input");
+const PDFdropZone = document.getElementById("pdf-input-zone");
+const PDFinput = document.getElementById("pdf-input");
 
 let js_file;
 let pdf_file;
@@ -10,15 +12,30 @@ let pdf_code;
 function js_click() {
   JSinput.click();
 }
+function pdf_click() {
+  PDFinput.click();
+}
+
 function js_receive() {
   js_file = JSinput.files[0];
   const js_reader = new FileReader();
-  reader.readAsText(js_file);
-  reader.onload = () => {
-    js_code = reader.result;
+  js_reader.readAsText(js_file);
+  js_reader.onload = () => {
+    js_code = js_reader.result;
     console.log(js_code);
+  };
+}
+function pdf_receive() {
+  pdf_file = PDFinput.files[0];
+  const pdf_reader = new FileReader();
+  pdf_reader.readAsText(pdf_file);
+  pdf_reader.onload = () => {
+    pdf_code = pdf_reader.result;
+    console.log(pdf_code);
   };
 }
 
 JSdropZone.addEventListener("click", js_click);
 JSinput.addEventListener("change", js_receive);
+PDFdropZone.addEventListener("click", pdf_click);
+PDFinput.addEventListener("change", pdf_receive);
