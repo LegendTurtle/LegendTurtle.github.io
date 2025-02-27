d1f7c065af8a989ae62264f66c6d9cef0eec18cf
Egor <legendturtle@gmail.com>
2025-02-27 21:29:06 +0300

    beta

--- js-to-pdf/script.js
+++ js-to-pdf/script.js
@@ -2,6 +2,7 @@ const JSdropZone = document.getElementById("js-input-zone");
 const JSinput = document.getElementById("js-input");
 document.getElementById("start").onclick = embed;
 
+let output;
 let js_file;
 let js_code;
 let is_js_receive = false;
@@ -11,6 +12,7 @@ function js_click() {
 }
 
 function js_receive() {
+  document.getElementById("result").style.display = "none";
   js_file = JSinput.files[0];
   const js_reader = new FileReader();
   js_reader.readAsText(js_file);
@@ -33,10 +35,14 @@ function embed() {
         request.responseText.slice(0, 360) +
         js_code +
         request.responseText.slice(403, -1);
-      document.getElementById("output").innerText = result;
     };
     request.send(null);
     is_js_receive = false;
+
+    document.getElementById("result").style.display = "flex";
+    document.getElementById("copy").onclick = function () {
+      navigator.clipboard.writeText(result);
+    };
   }
 }
 
