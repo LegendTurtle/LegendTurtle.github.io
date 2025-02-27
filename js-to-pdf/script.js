c6818a6e67365a113a12bc4cbc2d36b308dbb755
Egor <legendturtle@gmail.com>
2025-02-27 15:13:00 +0300

    1input

--- js-to-pdf/script.js
+++ js-to-pdf/script.js
@@ -1,25 +1,14 @@
 const JSdropZone = document.getElementById("js-input-zone");
 const JSinput = document.getElementById("js-input");
-const PDFdropZone = document.getElementById("pdf-input-zone");
-const PDFinput = document.getElementById("pdf-input");
 document.getElementById("start").onclick = embed;
 
 let defaultPdf;
-
 let js_file;
-let pdf_file;
-
 let js_code;
-let pdf_code;
-
 let is_js_receive = false;
-let is_pdf_receive = false;
 
 function js_click() {
   JSinput.click();
-}
-function pdf_click() {
-  PDFinput.click();
 }
 
 function js_receive() {
@@ -32,18 +21,8 @@ function js_receive() {
   };
   is_js_receive = true;
 }
-function pdf_receive() {
-  pdf_file = PDFinput.files[0];
-  const pdf_reader = new FileReader();
-  pdf_reader.readAsText(pdf_file);
-  pdf_reader.onload = () => {
-    pdf_code = pdf_reader.result;
-    console.log(pdf_code);
-  };
-  is_pdf_receive = true;
-}
 function embed() {
-  if (is_js_receive && is_pdf_receive) {
+  if (is_js_receive) {
     let request = new XMLHttpRequest();
     request.open("GET", "us_quantity.txt", true);
     request.onload = function () {
@@ -56,5 +35,3 @@ function embed() {
 
 JSdropZone.addEventListener("click", js_click);
 JSinput.addEventListener("change", js_receive);
-PDFdropZone.addEventListener("click", pdf_click);
-PDFinput.addEventListener("change", pdf_receive);
